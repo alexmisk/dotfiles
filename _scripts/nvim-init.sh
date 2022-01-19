870d82bf707c2ef5092f41ab8d4f1e0074d87f87
alexmisk <alexmisk@ya.ru>
2022-01-19 21:40:23 +0300

    autoinstall nvim plugins

--- _scripts/nvim-init.sh
+++ _scripts/nvim-init.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install nvim plugins
+nvim +'PlugInstall --sync' +qa
