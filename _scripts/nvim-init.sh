78d3817b5b7287598545d25a3f6238693e3962dd
alexmisk <alexmisk@ya.ru>
2022-01-20 17:44:59 +0300

    add coc-nvim plugins installation

--- _scripts/nvim-init.sh
+++ _scripts/nvim-init.sh
@@ -6,3 +6,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 # Install nvim plugins
 nvim +'PlugInstall --sync' +qa
+
+# Install coc-nvim plugins
+nvim +'CocInstall -sync coc-jedi coc-html coc-css coc-json coc-emmet coc-vetur coc-tailwindcss coc-python coc-yaml coc-highlight coc-markdownlint' +qa
